9f9f3d7c7edad62e5d99755242ab850db2bbc447
LeoMatus <nano981023@gmail.com>
2021-11-07 18:54:12 +0000

    add dataset to images, add matching cards function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
 //adds the flip class to the memory-cards
 function cardFlip() {
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard){
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else{
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log(firstCard, secondCard);
+
+        if (firstCard.dataset.image === secondCard.dataset.image){
+            //if cards match
+            firstCard.removeEventListener("click", cardFlip)
+            secondCard.removeEventListener("click", cardFlip)
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', cardFlip));
