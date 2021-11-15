332eac075ef878dc3a68b3c440451cfd24eabef6
LeoMatus <nano981023@gmail.com>
2021-11-15 23:24:42 +0000

    add comments to js and structure code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 
     restartGame();
-
+    
+    //makes the difficulty buttons clickable and it sets the number of attempts each button gives the player.
     for(let button of buttons){
         button.addEventListener("click", function(){
             if (this.classList.contains("green")){
@@ -31,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
             updateRemainingAttempts(maxAttempts - attempts);
 
+            //adds the selected CSS class to the clicked button
             selectedButton = this;
             this.classList.add("selected");
 
@@ -39,14 +41,20 @@ document.addEventListener("DOMContentLoaded", function(){
     }    
 })
 
+/**
+ * shows the number of attempts left on the web page
+ */
 function updateRemainingAttempts(remainingAttempts){   
     document.getElementById("remaining-attempts").innerText = remainingAttempts;
 }
 
-//adds the flip class to the memory-cards
+/**
+ * adds the flip class to the memory-cards which shows the memory card
+ */
 function cardFlip() {
     if (boardLock) return;
-        
+    
+    // adds the flip CSS class to the clicked card
     this.classList.toggle('flip');
     
     if (!hasFlippedCard){
@@ -66,8 +74,10 @@ function cardFlip() {
 
 }
 
-function checkCardsForMatch(test) {
-    
+/**
+ * Checks if two cards match eachother, disables the cards if they do match. If they dont match it unflips the cards and adds 1 attempt
+ */
+function checkCardsForMatch() {
     if (firstCard.dataset.image === secondCard.dataset.image){
         //if cards match
         disableMatchingCards();
@@ -76,16 +86,23 @@ function checkCardsForMatch(test) {
         if(attempts < maxAttempts){
             cardUnflip();
         } else{
-            gameOver()
+            gameOver();
         }
     }  
 }
 
+/**
+ * makes matching cards unclickable
+ */
 function disableMatchingCards(){
     firstCard.removeEventListener("click", cardFlip);
     secondCard.removeEventListener("click", cardFlip);
 }
 
+/**
+ * unflips cards that dont match
+ * makes cards clickable again
+ */
 function cardUnflip() {
     boardLock = true;
     //if cards dont match
@@ -97,11 +114,24 @@ function cardUnflip() {
     },2000);
 } 
 
+/**
+ * makes cards unclickable when max attempt limit is reached
+ */
 function gameOver(){
     cards.forEach(card => card.removeEventListener('click', cardFlip));
-    console.log("game over");
 }
 
+/**
+ * this function is called when the game needs to restart, both when you refresh the page and when a new difficulty is selected
+ * first it resets the number of attempts.
+ * Locks the board preventing any clicking on the cards.
+ * Flips all the cards so the backside is shown.
+ * It shuffles the board.
+ * Makes the cards clickable.
+ * Sets the new number of max attempts.
+ * Disables the board lock.
+ * Waits for the flip-animation to finish.
+ */
 function restartGame(){
     attempts = 0;
     boardLock = true;
@@ -116,14 +146,21 @@ function restartGame(){
     },1000);    
 }
 
+//makes all the cards flipable
 function activateCards(){
     cards.forEach(card => card.addEventListener('click', cardFlip));
 }
 
+/**
+ * unflips all cards
+ */
 function unflipAllCards(){
     cards.forEach(card => card.classList.remove("flip"));
 }
 
+/**
+ * shuffles the cards
+ */
 function shuffleBoard(){
     cards.forEach (card => {
         let randomPos = Math.floor(Math.random() * 24);
